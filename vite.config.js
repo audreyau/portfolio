fbc9f92f38a687ee20a4d7213255e2e2ec8020b0
audreyau <auau@oregonstate.edu>
2026-04-08 08:00:14 -0700

    Update vite base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // For GitHub Pages: set to '/<repo-name>/' if not using a custom domain.
-  // For a user/org site (username.github.io), use '/'.
-  base: '/',
+  base: '/portfolio/',
 })
